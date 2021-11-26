9b7810fffae7005b061124392c7cab259d6e9f53
Bilal Ahmed <info@bilal-ahmed.com>
2021-11-27 00:53:43 +0300

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
+import { enableScreens } from "react-native-screens";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import mealsReducer from "./store/reducers/meals";
+
+import MainNavigation from "./navigation/DrawerNavigation";
+
+enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer,
+});
+const store = createStore(rootReducer);
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    "open-sans": require("./assets/fonts/OpenSans-Bold.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <MainNavigation />
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
